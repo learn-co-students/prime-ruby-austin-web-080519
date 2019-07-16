003311c8e3fd06aa01d70ba18a8cb3b65794baa5
Anais Momin Ahmed <anaismahmed@uchicago.edu>
2019-07-16 02:14:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num == 2 || num == 3
+    return true
+  elsif num < 2
+    return false
+  else
+    tests = Array (2..Math.sqrt(num).ceil)
+    tests.each do |foo|
+      if num % foo == 0
+        return false
+      end
+    end
+  end
+  true
+end  
